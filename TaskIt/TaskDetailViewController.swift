9745ac08cc4f85df40616113eb39f1a49a5654eb
krzychukula <krzychukula@gmail.com>
2014-12-23 18:28:44 +0100

    new view controller

---
+++ TaskIt/TaskDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TaskDetailViewController.swift
+//  TaskIt
+//
+//  Created by Krzysztof Kula on 23/12/14.
+//  Copyright (c) 2014 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+             
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
