ca4622544d51dfb923848d9b74d33a56ef9f562e
krzychukula <krzychukula@gmail.com>
2014-12-25 11:02:03 +0100

    show and hide add task vc

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -48,6 +48,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
     }
+    
+    @IBAction func addButtonTapped(sender: UIBarButtonItem) {
+        self.performSegueWithIdentifier("showTaskAdd", sender: self)
+    }
+    
 
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
