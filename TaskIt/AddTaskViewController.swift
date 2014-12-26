707384aa7d7957055dd3d1bc79766b5f2a39c271
krzychukula <krzychukula@gmail.com>
2014-12-26 11:17:53 +0100

    add Task

--- TaskIt/AddTaskViewController.swift
+++ TaskIt/AddTaskViewController.swift
@@ -32,4 +32,9 @@ class AddTaskViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func addTaskButtonTapped(sender: UIButton) {
+        var task = TaskModel(task: taskTextField.text, subTask: subtaskTextField.text, date: dueDatePicker.date)
+        mainVC.taskArray.append(task)
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
