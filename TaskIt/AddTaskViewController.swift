b00f7cd4f02c1b761b207e4c65a267cfe072877b
krzychukula <krzychukula@gmail.com>
2014-12-26 18:24:03 +0100

    create entity and fetch all entities

--- TaskIt/AddTaskViewController.swift
+++ TaskIt/AddTaskViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTaskViewController: UIViewController {
     
@@ -33,8 +34,29 @@ class AddTaskViewController: UIViewController {
     }
 
     @IBAction func addTaskButtonTapped(sender: UIButton) {
-        var task = TaskModel(task: taskTextField.text, subTask: subtaskTextField.text, date: dueDatePicker.date, completed: false)
-        mainVC.baseArray[0].append(task)
+        
+        let appDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        let managedObjectContext = appDelegate.managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("TaskModel", inManagedObjectContext: managedObjectContext!)
+        let task = TaskModel(entity:entityDescription!, insertIntoManagedObjectContext:managedObjectContext!)
+        
+        task.task = taskTextField.text
+        task.subtask = subtaskTextField.text
+        task.completed = false
+        task.date = dueDatePicker.date
+        
+        appDelegate.saveContext()
+        
+        var request = NSFetchRequest(entityName: "TaskModel")
+        var error:NSError?
+        
+        var results:NSArray = managedObjectContext!.executeFetchRequest(request, error: &error)!
+        
+        for res in results {
+            println(res)
+        }
+        
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
