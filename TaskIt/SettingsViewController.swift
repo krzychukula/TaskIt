f2ad64791df76116f16b8809cc31d64241b40ab2
krzychukula <krzychukula@gmail.com>
2015-06-17 20:00:03 +0200

    setup table views and done bar button item

--- TaskIt/SettingsViewController.swift
+++ TaskIt/SettingsViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let kVersionNumber = "1.0"
 
     @IBOutlet weak var capitalizeTableView: UITableView!
     @IBOutlet weak var completeNewTodoTableView: UITableView!
@@ -19,6 +21,20 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.capitalizeTableView.delegate = self
+        self.capitalizeTableView.dataSource = self
+        self.capitalizeTableView.scrollEnabled = false
+        
+        self.completeNewTodoTableView.delegate = self
+        self.completeNewTodoTableView.dataSource = self
+        self.completeNewTodoTableView.scrollEnabled = false
+        
+        self.title = "Settings"
+        self.versionLabel.text = kVersionNumber
+        
+        var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("doneBarButtonItemPressed"))
+        self.navigationItem.leftBarButtonItem = doneButton;
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,5 +42,20 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func doneBarButtonItemPressed(){
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    //UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
